47613e2e04abea78cc635556f3a3815e7f3a82f9
Minji Chung <jmjcap123@gmail.com>
2024-01-05 18:17:42 +0900

    ✨ Feat : 종료 후 화면 출력 기능 추가

--- spaceshooter/spaceShooter.py
+++ spaceshooter/spaceShooter.py
@@ -47,8 +47,8 @@ YELLOW = (255, 255, 0)
 ## initialize pygame and create window
 pygame.init()
 pygame.mixer.init()  ## For sound
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Space Shooter")
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) # GUI 창을 설정
+pygame.display.set_caption("Space Shooter") # 창이름 설정
 clock = pygame.time.Clock()     ## For syncing the FPS
 ###############################
 
@@ -88,6 +88,35 @@ def main_menu():
     screen.fill(BLACK)
     draw_text(screen, "GET READY!", 40, WIDTH/2, HEIGHT/2)
     pygame.display.update()
+
+def exit_menu():  #종료 화면 추가
+    global screen
+
+    menu_song = pygame.mixer.music.load(path.join(sound_folder, "menu.ogg"))
+    pygame.mixer.music.play(-1)
+
+    title = pygame.image.load(path.join(img_dir, "starfield.png")).convert()
+    title = pygame.transform.scale(title, (WIDTH, HEIGHT), screen)
+
+    screen.blit(title, (0,0))   
+    pygame.display.update()  
+
+    while True:                       
+        ev = pygame.event.poll()
+        if ev.type == pygame.KEYDOWN:    
+            if ev.key == pygame.K_RETURN:
+                break
+            elif ev.key == pygame.K_q:
+                pygame.quit()
+                quit()
+        elif ev.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        else:
+            draw_text(screen, "THE END", 60, WIDTH/2, (HEIGHT/3)-30)
+            draw_text(screen, "Press [ENTER] To Begin", 30, WIDTH/2, HEIGHT/2)
+            draw_text(screen, "or [Q] To Quit", 30, WIDTH/2, (HEIGHT/2)+40)
+            pygame.display.update()
     
 
 def draw_text(surf, text, size, x, y):
